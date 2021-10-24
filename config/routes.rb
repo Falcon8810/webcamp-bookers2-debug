54091404b8208405f900afff43066039df3faa95
Falcon8810 <hayato.sa9211@gmail.com>
2021-10-24 14:35:44 +0000

    error,post,get??

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
-  
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
   end
-
 
 end
