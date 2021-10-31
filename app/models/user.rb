b10398c09fdc0fb97edb945278a420249812b162
Falcon8810 <hayato.sa9211@gmail.com>
2021-10-31 14:36:50 +0000

    [Update]model_User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,19 @@ class User < ApplicationRecord
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following_user, through: :follower, source: :followed
-  has_many :follwer_user, through: :followed, source: :follower
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(follower_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followded_id: user_id).destroy
+  end
+
+  def following?(user)
+    following.user.include?(user)
+  end
 
   attachment :profile_image, destroy: false
 
