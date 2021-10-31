c355e284c53f1738a0373213ebc4845b1b09f058
Falcon8810 <hayato.sa9211@gmail.com>
2021-10-31 13:12:29 +0000

    [Update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follwer_user, through: :followed, source: :follower
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
