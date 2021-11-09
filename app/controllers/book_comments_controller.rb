0f050c6d525f2c21a55cfd10dfd828db74640c1c
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-09 12:52:12 +0000

    [Update]応用課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,6 +7,10 @@ class BookCommentsController < ApplicationController
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
     @book_comment_new = BookComment.new
+    #９行目自分で考えた。
+    #book_comments/create.js.erbの3行目の記述も必要ないが、
+    #この９行目を追加することで実装が可能に
+    
     if @book_comment.save
       # redirect_to book_path(@book.id)
     else
