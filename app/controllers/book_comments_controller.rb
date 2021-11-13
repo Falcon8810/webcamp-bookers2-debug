aad8b2ea85be141e3a544508669df327d7d2a441
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-13 10:21:00 +0000

    [Update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,11 +6,7 @@ class BookCommentsController < ApplicationController
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
-    @book_comment_new = BookComment.new
-    #９行目自分で考えた。
-    #book_comments/create.js.erbの3行目の記述も必要ないが、
-    #この９行目を追加することで実装が可能に
-    #こういう発見がすごく大事だと思う
+
 
     if @book_comment.save
     else
