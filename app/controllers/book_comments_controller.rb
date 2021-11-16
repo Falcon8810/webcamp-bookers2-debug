a1f2e058b17eccd276bf61a2771d6c46651b9f20
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-16 08:50:27 +0000

    [Update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -25,9 +25,6 @@ class BookCommentsController < ApplicationController
     book_comment.destroy
   end
 
-  private
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
+
 
 end
